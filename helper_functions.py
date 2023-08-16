ca609826a2d252f92764bcdf2a38f8f88a941f69
Renee Noble <renee.noble@hotmail.com>
2023-08-16 07:37:24 +0000

    answers

--- helper_functions.py
+++ helper_functions.py
@@ -1,11 +1,12 @@
-# create a function that capitaises names
+# create a function that capitalises names
 def capitalise_name(name):
-    # TODO: Capitalise the name
+    return name.capitalize()
 
 # Create a function that gives back a list of colours, based on a specified length passed in as an argument
 def get_colours(length):
-    # TODO: Create a list of colours
+    colours = ["red", "green", "blue", "yellow", "black", "white", "purple", "orange"]
+    return colours[:length]
 
 # Create a function that takes in a list of names and returns a list of names that start with a specified letter
 def get_names_starting_with(names, letter):
-    # ToDO: Create a list of names that start with a specified letter
+    return [name for name in names if name.startswith(letter)]
