9a23fcee137e99ab4304951ae3f247b2d047ab7c
Renee Noble <renee.noble@hotmail.com>
2023-08-16 05:10:45 +0000

    functions to fill out

---
+++ helper_functions.py
@@ -0,0 +1,11 @@
+# create a function that capitaises names
+def capitalise_name(name):
+    # TODO: Capitalise the name
+
+# Create a function that gives back a list of colours, based on a specified length passed in as an argument
+def get_colours(length):
+    # TODO: Create a list of colours
+
+# Create a function that takes in a list of names and returns a list of names that start with a specified letter
+def get_names_starting_with(names, letter):
+    # ToDO: Create a list of names that start with a specified letter
